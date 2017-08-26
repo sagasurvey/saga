212d6a0ed7cc615f979e83adca9278b48dd348c5
Yao-Yuan Mao <yymao.astro@gmail.com>
2017-08-26 18:02:22 -0400

    working on TargetSelection, adding GMM

---
+++ SAGA/targets/gmm.py
@@ -0,0 +1,68 @@
+import os
+import numpy as np
+from scipy.misc import logsumexp
+
+_colors = ('ug', 'gr', 'ri', 'iz')
+
+# compute distance from GMM model with diagonal or full covariances
+def _GMMlogposterior(y, yerr, xmap, xmean, xcovar):
+    """
+    Examples
+    --------
+    # cols and colerrs are arrays of nobj rows and 4 columns containing the
+    # u-g, g-r, r-i and i-z colors and color-errors.
+
+    allpost_nosat = GMMlogposterior(cols[:, :], colerrs[:, :], xamp_nosat, xmean_nosat, xcovar_nosat)
+    allpost_sat = GMMlogposterior(cols[:, :], colerrs[:, :], xamp_sat, xmean_sat, xcovar_sat)
+    norms = allpost_nosat + allpost_sat
+
+    # Normalize the probabilities like in a binary Bayes classifier.
+
+    allpost_nosat /= norms
+    allpost_sat /= norms
+    """
+    assert y.shape[1] == xmean.shape[1]
+    assert xmean.shape[0] == xcovar.shape[0]
+    assert xmean.shape[1] == xcovar.shape[1]
+    nobj = y.shape[0]
+    ndim = xcovar.shape[1]
+    dys = y[:, None, :] - xmean[None, :, :] # nobj * ncomp * ndim
+    if len(xcovar.shape) == 2:
+        covs = yerr[:, None, :]**2 + xcovar[None, :, :]
+        lnprobs = - 0.5 * np.sum(dys**2 / covs, axis=2) - 0.5*np.sum(np.log(covs), axis=2)
+    if len(xcovar.shape) == 3:
+        eyes = np.repeat(np.eye(ndim), nobj).reshape((ndim, ndim, 1, nobj)).T
+        covs = yerr[:, None, :, None]**2 * eyes + xcovar[None, :, :, :]
+        temp = np.sum(np.linalg.inv(covs) * dys[:, :, None, :], axis=3)
+        lnprobs = - 0.5 * np.sum(temp * dys, axis=2) - 0.5*np.log(np.linalg.det(covs))
+    lnprobs += np.log(xmap[None, :])
+    return np.exp(logsumexp(lnprobs, axis=1)) # nobj
+
+
+def _change_table_format(table, cols):
+    if table.masked:
+        return np.vstack((table[c].data.data for c in cols)).T
+    else:
+        return np.vstack((table[c].data for c in cols)).T
+
+
+def calc_satellite_probability(base, model_parameters):
+
+    colors = _change_table_format(base, _colors)
+    colors_err = _change_table_format(base, ('{}_err'.format(c) for c in _colors))
+
+    p_notsat = _GMMlogposterior(colors, colors_err,
+                                model_parameters['xamp_nosat'],
+                                model_parameters['xmean_nosat'],
+                                model_parameters['xcovar_nosat'])
+
+    p_sat = _GMMlogposterior(colors, colors_err,
+                                model_parameters['xamp_sat'],
+                                model_parameters['xmean_sat'],
+                                model_parameters['xcovar_sat'])
+
+    p_notsat += p_sat
+    p_sat /= p_notsat
+    p_sat[p_sat == np.inf] = 1.0
+    p_sat[~np.isfinite(p_sat)] = 0.0
+    return p_sat
--- SAGA/targets/target_selection.py
+++ SAGA/targets/target_selection.py
@@ -1,22 +1,35 @@
+from easyquery import Query
 from ..objects import ObjectCatalog
 from ..objects import cuts as C
-from ..utils import fill_values_by_query
+from ..utils import fill_values_by_query, get_empty_str_array
+from .gmm import calc_satellite_probability
+
+_colors = ['ug', 'gr', 'ri', 'iz']
 
 class TargetSelection(object):
     def __init__(self, database):
         self._database = database
         self._objects = ObjectCatalog(self._database)
 
-    def select_preliminary_targets(self, host_id):
-        base = self._objects.load(hosts=host_id, query=(Q.basic_cut & ~Q.has_spec))
+    def select_preliminary_targets(self, host_id, return_columns=['OBJID', 'RA', 'DEC']):
+        columns = list(set(return_columns + _colors + ['{}_err'.format(c) for c in _colors] + ['OBJID']))
+
+        base = self._objects.load(hosts=host_id, has_spec=False, cuts=C.basic_cut, columns=columns)
 
         base['TARGETING_LABEL'] = get_empty_str_array(len(base))
         base['TARGETING_SCORE'] = 9999.0
 
         n_bright = fill_values_by_query(base, C.sdss_limit, {'TARGETING_LABEL':'BRIGHT', 'TARGETING_SCORE': 0.0})
 
-        #n_risa = fill_values_by_query(base,  C ,{'TARGETING_LABEL':'RISA', 'TARGETING_SCORE': 1.0})
-        #n_high_p =
+        risa_objid = self._database._table['risa_objects'].read()['OBJID']
+        n_risa = fill_values_by_query(base,  Query((lambda x: np.in1d(x, risa_objid), 'OBJID')),
+                                      {'TARGETING_LABEL':'RISA', 'TARGETING_SCORE': 1.0})
 
+        p = calc_satellite_probability(base, self._database._table['gmm_model_para'].read())
+        p_mask = (p > 0.5)
+        base['TARGETING_LABEL'][p_mask] = 'HIGH_P_GMM'
+        base['TARGETING_SCORE'][p_mask] = 3.0 - p[p_mask]
 
-        return
+        #TODO: finish this
+
+        return base
