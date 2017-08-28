0172a1686ffbfa53ee430db44e83682800afb581
Yao-Yuan Mao <yymao.astro@gmail.com>
2017-08-28 16:19:11 -0400

    add satellites_named to Database

--- SAGA/database/core.py
+++ SAGA/database/core.py
@@ -146,6 +146,7 @@ class Database(object):
             'hosts_no_sdss_flags': GoogleSheets('1b3k2eyFjHFDtmHce1xi6JKuj3ATOWYduTBFftx5oPp8', 1471095077),
             'objects_to_remove': GoogleSheets('1Y3nO7VyU4jDiBPawCs8wJQt2s_PIAKRj-HSrmcWeQZo', 1379081675, header_start=1),
             'objects_to_add': GoogleSheets('1Y3nO7VyU4jDiBPawCs8wJQt2s_PIAKRj-HSrmcWeQZo', 286645731, header_start=1),
+            'satellites_named': GoogleSheets('1GJYuhqfKeuJr-IyyGF_NDLb_ezL6zBiX2aeZFHHPr_s', 1),
         }
 
         if self._root_dir is not None:
