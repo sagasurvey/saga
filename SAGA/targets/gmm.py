31525b0ca5215d2881ac446f547149c5d4e8a4ec
Yao-Yuan Mao <yymao.astro@gmail.com>
2018-04-25 19:48:59 -0400

    better calc_model1_prob and bug fix

--- SAGA/targets/gmm.py
+++ SAGA/targets/gmm.py
@@ -21,11 +21,11 @@ param_labels_nosat = ('xmean_nosat', 'xcovar_nosat', 'xamp_nosat')
 
 
 class XDGMM(object):
-    def __init__(self, n_components=1, **kwargs):
+    def __init__(self, n_components=1, initial_iter=20, **kwargs):
         if not _XDGMM_AVAILABLE:
             raise RuntimeError('You need to have both sklearn and extreme_deconvolution installed to use XDGMM')
         self._kwargs = kwargs
-        self._gmm = GaussianMixture(int(n_components), max_iter=10, covariance_type='full')
+        self._gmm = GaussianMixture(int(n_components), max_iter=int(initial_iter), covariance_type='full')
 
     def fit(self, data, data_cov, **kwargs):
         self._gmm.fit(data)
@@ -67,17 +67,16 @@ def get_input_data(catalog, colors=None, bands=None, include_covariance=True):
 def check_calc_log_likelihood_input(data, data_cov, gmm_means, gmm_covs, gmm_weights):
     assert 1 == gmm_weights.ndim
     assert 2 == data.ndim == gmm_means.ndim
-    assert 3 == data_cov.ndim
-    assert gmm_covs.ndim in (2, 3)
+    assert 3 == data_cov.ndim == gmm_covs.ndim
     assert data.shape[0] == data_cov.shape[0]
     assert gmm_means.shape[0] == gmm_covs.shape[0] == gmm_weights.shape[0]
     assert data.shape[1] == data_cov.shape[1] == data_cov.shape[2] == gmm_means.shape[1] == gmm_covs.shape[1] == gmm_covs.shape[2]
 
 
 def calc_log_likelihood(data, data_cov, gmm_means, gmm_covs, gmm_weights):
-    check_calc_log_likelihood_input(data, data_cov, gmm_means, gmm_covs, gmm_weights)
     if gmm_covs.ndim == 2:
         gmm_covs = np.stack((np.diag(c) for c in gmm_covs))
+    check_calc_log_likelihood_input(data, data_cov, gmm_means, gmm_covs, gmm_weights)
     d = data[:, np.newaxis] - gmm_means
     cov = data_cov[:, np.newaxis] + gmm_covs
     tmp_result = np.einsum('...i,...ij,...j->...', d, np.linalg.inv(cov), d)
@@ -88,24 +87,24 @@ def calc_log_likelihood(data, data_cov, gmm_means, gmm_covs, gmm_weights):
 
 
 def calc_model1_prob(data, data_cov, model_params, priors=None):
-    p = np.exp(np.stack((calc_log_likelihood(data, data_cov, *model_params_this) for model_params_this in model_params)))
+    p = np.stack((calc_log_likelihood(data, data_cov, *model_params_this) for model_params_this in model_params))
     if priors is not None:
         priors = np.asarray(priors)
-        assert len(priors) == len(p)
-        assert (priors >= 0).all() and priors.sum() > 0
-        p *= priors[:, np.newaxis]
-    p_total = p.sum(axis=0)
-    mask = (p_total == 0) | (~np.isfinite(p_total)) | (~np.isfinite(p[0]))
-    p_total[mask] = 1.0
-    p_out = p[0] / p_total
-    p_out[mask] = (1.0 / len(p)) if priors is None else (priors[0] / priors.sum())
-    return p_out
+        assert len(priors) == len(p) and (priors > 0).all()
+    p = p[0] - logsumexp(p.T, axis=1, b=priors)
+    if priors is not None:
+        p += np.log(priors[0])
+    p = np.exp(p, out=p)
+    return p
 
 
-def calc_gmm_satellite_probability(base, model_parameters, p_sat_prior=0.5, include_covariance=True):
+def calc_gmm_satellite_probability(base, model_parameters, p_sat_prior=None, include_covariance=True):
     data, data_cov = get_input_data(base, include_covariance=include_covariance)
     model_params = (
         tuple(model_parameters[k] for k in param_labels_sat),
         tuple(model_parameters[k] for k in param_labels_nosat)
     )
-    return calc_model1_prob(data, data_cov, model_params, [p_sat_prior, 1-p_sat_prior])
+    return calc_model1_prob(data,
+                            data_cov,
+                            model_params,
+                            None if p_sat_prior is None else [p_sat_prior, 1-p_sat_prior])
