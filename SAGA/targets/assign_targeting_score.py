625d0b5e5c78258cde085767b01f9b07d6480bd9
Yao-Yuan Mao <yymao.astro@gmail.com>
2018-06-07 12:03:17 -0400

    new assign_targeting_score_merged_catalog

--- SAGA/targets/assign_targeting_score.py
+++ SAGA/targets/assign_targeting_score.py
@@ -5,10 +5,10 @@ from itertools import chain
 import numpy as np
 from easyquery import Query
 from ..objects import cuts as C
-from ..utils import fill_values_by_query, get_sdss_bands, get_sdss_colors, get_des_bands
+from ..utils import fill_values_by_query, get_sdss_bands, get_sdss_colors, get_all_colors
 from .gmm import calc_gmm_satellite_probability, calc_log_likelihood, get_input_data, param_labels_nosat
 
-__all__ = ['assign_targeting_score', 'calc_simple_satellite_probability', 'calc_gmm_satellite_probability']
+__all__ = ['assign_targeting_score_mmt2018', 'calc_simple_satellite_probability', 'calc_gmm_satellite_probability']
 
 
 COLUMNS_USED = list(set(chain(C.COLUMNS_USED, ['TELNAME'],
@@ -36,8 +36,8 @@ def ensure_proper_prob(p):
     return p
 
 
-def assign_targeting_score(base, manual_selected_objids=None,
-                           gmm_parameters=None, version=2):
+def assign_targeting_score_mmt2018(base, manual_selected_objids=None,
+                                   gmm_parameters=None):
     """
     Last updated: 05/07/2018
      100 Human selection and Special targets
@@ -56,76 +56,26 @@ def assign_targeting_score(base, manual_selected_objids=None,
     1300 Not clean
     1400 Has spec but not a satellite
     """
+
     base['P_simple'] = ensure_proper_prob(calc_simple_satellite_probability(base))
+    base['P_GMM_sdss'] = ensure_proper_prob(calc_gmm_satellite_probability(base, gmm_parameters))
+    base['P_GMM'] = base['P_GMM_sdss']
+    base['log_L_GMM'] = calc_log_likelihood(*get_input_data(base), *(gmm_parameters[n] for n in param_labels_nosat))
 
-    if version == 2 and 'u_mag_sdss' in base.colnames:
-        for color in get_sdss_colors():
-            base[color+'_sdss'] = base['{}_mag_sdss'.format(color[0])] - base['{}_mag_sdss'.format(color[1])]
-            base[color+'_err_sdss'] = np.hypot(base['{}_err_sdss'.format(color[0])], base['{}_err_sdss'.format(color[1])])
-
-        base['P_GMM'] = ensure_proper_prob(calc_gmm_satellite_probability(
-            base,
-            gmm_parameters,
-            mag_err_postfix='_err_sdss',
-            color_postfix='_sdss',
-            color_err_postfix='_err_sdss',
-        ))
-        base['log_L_GMM'] = calc_log_likelihood(
-            *get_input_data(
-                base,
-                mag_err_postfix='_err_sdss',
-                color_postfix='_sdss',
-                color_err_postfix='_err_sdss',
-            ),
-            *(gmm_parameters[n] for n in param_labels_nosat)
-        )
-        base['GMM_valid'] = (base['OBJID_sdss'] != -1)
-    else:
-        bands = get_des_bands() if version > 1 else get_sdss_bands()
-        base['P_GMM'] = ensure_proper_prob(calc_gmm_satellite_probability(
-            base,
-            gmm_parameters,
-            bands=bands,
-        ))
-        base['log_L_GMM'] = calc_log_likelihood(
-            *get_input_data(
-                base,
-                bands=bands,
-            ),
-            *(gmm_parameters[n] for n in param_labels_nosat)
-        )
-        base['GMM_valid'] = True
-
-    if version == 1:
-        is_galaxy = C.is_galaxy
-        is_clean = C.is_clean
-        basic_cut = C.gri_cut & C.fibermag_r_cut & C.is_clean & C.is_galaxy & (~C.has_spec)
-    else:
-        is_galaxy = C.is_galaxy2
-        is_clean = C.is_clean2
-        basic_cut = C.gri_cut & C.is_clean2 & C.is_galaxy2 & (~C.has_spec) & Query('r_mag < 21.25')
-
-    within_host = basic_cut & C.faint_end_limit & C.sat_rcut
+    basic_cut = C.gri_cut & C.fibermag_r_cut & C.is_clean & C.is_galaxy & (~C.has_spec)
+    within_host =  basic_cut & C.faint_end_limit & C.sat_rcut
     outwith_host = basic_cut & C.faint_end_limit & (~C.sat_rcut)
 
-    veryhigh_p = Query('P_GMM >= 0.95', 'log_L_GMM >= -7', 'GMM_valid')
-    high_p = Query('P_GMM >= 0.6', 'log_L_GMM >= -7', 'GMM_valid') | Query('log_L_GMM < -7', 'ri-abs(ri_err) < -0.25', 'GMM_valid')
-    if 'u_mag_sdss' in base.colnames:
-        # TODO: need to implement some grz cut for decals
-        median_p = Query('(gr_sdss-abs(gr_err_sdss))*0.65+(ri_sdss-abs(ri_err_sdss)) < 0.6',
-                         '-(ug_sdss+abs(ug_err_sdss))*0.15+(ri_sdss-abs(ri_err_sdss)) < 0.08',
-                         '-(ug_sdss+abs(ug_err_sdss))*0.1+(gr_sdss-abs(gr_err_sdss)) < 0.5')
-    elif version == 1:
-        median_p = Query('(gr-abs(gr_err))*0.65+(ri-abs(ri_err)) < 0.6',
-                         '-(ug+abs(ug_err))*0.15+(ri-abs(ri_err)) < 0.08',
-                         '-(ug+abs(ug_err))*0.1+(gr-abs(gr_err)) < 0.5')
-    else:
-        median_p = Query('(gr-abs(gr_err))*0.65+(ri-abs(ri_err)) < 0.6')
+    veryhigh_p = Query('P_GMM_sdss >= 0.95', 'log_L_GMM >= -7')
+    high_p = Query('P_GMM_sdss >= 0.6', 'log_L_GMM >= -7') | Query('log_L_GMM < -7', 'ri-abs(ri_err) < -0.25')
+    median_p = Query('-(ug+abs(ug_err))*0.15+(ri-abs(ri_err)) < 0.08',
+                     '(gr-abs(gr_err))*0.65+(ri-abs(ri_err)) < 0.6',
+                     '-(ug+abs(ug_err))*0.1+(gr-abs(gr_err)) < 0.5')
 
     base['TARGETING_SCORE'] = 1000
     fill_values_by_query(base, ~basic_cut, {'TARGETING_SCORE': 1100})
-    fill_values_by_query(base, ~is_galaxy, {'TARGETING_SCORE': 1200})
-    fill_values_by_query(base, ~is_clean, {'TARGETING_SCORE': 1300})
+    fill_values_by_query(base, ~C.is_galaxy, {'TARGETING_SCORE': 1200})
+    fill_values_by_query(base, ~C.is_clean, {'TARGETING_SCORE': 1300})
     fill_values_by_query(base, C.has_spec, {'TARGETING_SCORE': 1400})
 
     fill_values_by_query(base, outwith_host, {'TARGETING_SCORE': 900})
@@ -144,16 +94,132 @@ def assign_targeting_score(base, manual_selected_objids=None,
         need_random_selection = need_random_selection[random_mask]
     base['TARGETING_SCORE'][need_random_selection] = 600
 
-    base['TARGETING_SCORE'] += (np.round((1.0 - base['P_GMM'])*80.0).astype(np.int) + 10)
+    base['TARGETING_SCORE'] += (np.round((1.0 - base['P_GMM_sdss'])*80.0).astype(np.int) + 10)
 
-    fill_values_by_query(base,
-                         Query(C.is_sat, (lambda x: (x != 'AAT') & (x != 'MMT'), 'TELNAME')),
-                         {'TARGETING_SCORE': 150})
+    fill_values_by_query(base, C.is_sat, {'TARGETING_SCORE': 150})
 
     if manual_selected_objids:
         fill_values_by_query(base, \
                 Query((lambda x: np.in1d(x, manual_selected_objids), 'OBJID')), \
                 {'TARGETING_SCORE': 100})
+
+    base.sort('TARGETING_SCORE')
+    return base
+
+
+def assign_targeting_score_merged_catalog(base, gmm_parameters_dict,
+                                          testing=False):
+    """
+    Last updated: 06/07/2018
+     100 Human selection and Special targets
+     150 satellites
+     200 within host,  r < 17.77, gri cuts
+     300 within host,  r < 20.75, high p_GMM or GMM outliers, gri cuts
+     400 within host,  r < 20.75, high-proirity + gri cuts
+     500 very high p_GMM
+     600 within host,  r < 20.75, gri cuts, random selection of 50
+     700 outwith host, r < 17.77 or high p_GMM
+     800 within host,  r < 20.75, gri cuts, everything else
+     900 outwith host, r < 20.75, gri cuts
+    1000 everything else
+    1100 Not in gri/fibermag_r_cut
+    1200 Not galaxy
+    1300 Not clean
+    1400 Has spec but not a satellite
+    """
+
+    valid_i_mag = Query('i_mag > 0', 'i_mag < 30')
+    grz_cut = Query('gr-abs(gr_err) < 0.85', 'rz-rz_err < 1')
+    gri_or_grz_cut = Query(C.gri_cut, valid_i_mag) | Query(grz_cut, ~valid_i_mag)
+    basic_cut = gri_or_grz_cut & C.is_clean2 & C.is_galaxy2 & Query('r_mag < 21')
+    if not testing:
+        basic_cut &= (~C.has_spec)
+    base_clean = basic_cut.filter(base)
+    base_clean['index'] = np.flatnonzero(basic_cut.mask(base))
+
+    base['TARGETING_SCORE'] = 1000
+
+    surveys = [col[6:] for col in base.colnames if col.startswith('OBJID_')]
+
+    for survey in surveys:
+        postfix = '_' + survey
+        base_this = Query('OBJID{} != -1'.format(postfix)).filter(base_clean)
+
+        for color in get_all_colors():
+            b1, b2 = color
+            n1 = ''.join((b1, '_mag', postfix))
+            n2 = ''.join((b2, '_mag', postfix))
+            if n1 not in base_this.colnames or n2 not in base_this.colnames:
+                continue
+            base_this[color] = base[n1] - base[n2]
+            base_this[color + '_err'] = np.hypot(
+                base[''.join((b1, '_err', postfix))],
+                base[''.join((b2, '_err', postfix))],
+            )
+
+        if survey in gmm_parameters_dict:
+            gmm_parameters = gmm_parameters_dict[survey]
+            base_this['P_GMM'] = ensure_proper_prob(calc_gmm_satellite_probability(
+                base_this,
+                gmm_parameters,
+                mag_err_postfix='_err'+postfix,
+                color_postfix=postfix,
+                color_err_postfix='_err'+postfix,
+            ))
+            base_this['log_L_GMM'] = calc_log_likelihood(
+                *get_input_data(
+                    base_this,
+                    mag_err_postfix='_err'+postfix,
+                    color_postfix=postfix,
+                    color_err_postfix='_err'+postfix,
+                ),
+                *(gmm_parameters[n] for n in param_labels_nosat)
+            )
+        else:
+            base_this['P_GMM'] = 0
+            base_this['log_L_GMM'] = 0
+
+        if survey == 'decals':
+            priority_cut = Query('(gr-abs(gr_err))*0.65+(rz-abs(rz_err)) < 0.6') # TODO fix this
+        else:
+            priority_cut = Query('(gr-abs(gr_err))*0.65+(ri-abs(ri_err)) < 0.6')
+            if survey == 'sdss':
+                priority_cut &= Query('-(ug+abs(ug_err))*0.15+(ri-abs(ri_err)) < 0.08',
+                                      '-(ug+abs(ug_err))*0.1+(gr-abs(gr_err)) < 0.5')
+
+        veryhigh_p = Query('P_GMM >= 0.95', 'log_L_GMM >= -7')
+        high_p = Query('P_GMM >= 0.6', 'log_L_GMM >= -7') | Query('log_L_GMM < -7', 'ri-abs(ri_err) < -0.25')
+
+        base_this['TARGETING_SCORE'] = 900
+        fill_values_by_query(base_this, C.sat_rcut, {'TARGETING_SCORE': 800})
+        fill_values_by_query(base_this, C.sdss_limit | high_p, {'TARGETING_SCORE': 700})
+        fill_values_by_query(base_this, veryhigh_p, {'TARGETING_SCORE': 500})
+        fill_values_by_query(base_this, C.sat_rcut & priority_cut & C.faint_end_limit, {'TARGETING_SCORE': 400})
+        fill_values_by_query(base_this, C.sat_rcut & high_p & C.faint_end_limit, {'TARGETING_SCORE': 300})
+        fill_values_by_query(base_this, C.sat_rcut & C.sdss_limit, {'TARGETING_SCORE': 200})
+        base_this['TARGETING_SCORE'] += (np.round((1.0 - base_this['P_GMM'])*80.0).astype(np.int) + 10)
+
+        to_update_mask = base_this['TARGETING_SCORE'] < base['TARGETING_SCORE'][base_this['index']]
+        to_update_idx = base_this['index'][to_update_mask]
+        base['TARGETING_SCORE'][to_update_idx] = base_this['TARGETING_SCORE'][to_update_mask]
+
+    need_random_selection = np.flatnonzero(Query('TARGETING_SCORE >= 800', 'TARGETING_SCORE < 900').mask(base))
+    if len(need_random_selection) > 50:
+        random_mask = np.zeros(len(need_random_selection), dtype=np.bool)
+        random_mask[:50] = True
+        np.random.RandomState(123).shuffle(random_mask)
+        need_random_selection = need_random_selection[random_mask]
+    base['TARGETING_SCORE'][need_random_selection] -= 200
+
+    fill_values_by_query(base, ~basic_cut, {'TARGETING_SCORE': 1100})
+    fill_values_by_query(base, ~C.is_galaxy2, {'TARGETING_SCORE': 1200})
+    fill_values_by_query(base, ~C.is_clean2, {'TARGETING_SCORE': 1300})
+    if not testing:
+        fill_values_by_query(base, C.has_spec, {'TARGETING_SCORE': 1400})
+
+    fill_values_by_query(base,
+                         Query(C.is_sat, (lambda x: (x != 'AAT') & (x != 'MMT'), 'TELNAME')),
+                         {'TARGETING_SCORE': 150})
 
     base.sort('TARGETING_SCORE')
     return base
