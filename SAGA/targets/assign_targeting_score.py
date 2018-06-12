cee89f1ba0cfadac5ef9afdb9c2c4b39db096933
Yao-Yuan Mao <yymao.astro@gmail.com>
2018-06-11 18:53:52 -0600

    fix target assignement issue

--- SAGA/targets/assign_targeting_score.py
+++ SAGA/targets/assign_targeting_score.py
@@ -220,14 +220,24 @@ def assign_targeting_score_v2(base, manual_selected_objids=None,
     if not testing:
         fill_values_by_query(base, C.has_spec, {'TARGETING_SCORE': 1400})
 
-    fill_values_by_query(base,
-                         Query(C.is_sat, (lambda x: (x != 'AAT') & (x != 'MMT'), 'TELNAME')),
-                         {'TARGETING_SCORE': 150})
+    fill_values_by_query(
+        base,
+        Query('ZQUALITY == 2', 'SPEC_Z < 0.05'),
+        {'TARGETING_SCORE': 180}
+    )
+
+    fill_values_by_query(
+        base,
+        Query(C.is_sat, (lambda x: (x != 'AAT') & (x != 'MMT'), 'TELNAME')),
+        {'TARGETING_SCORE': 150}
+    )
 
     if manual_selected_objids is not None:
-        fill_values_by_query(base, \
-                Query((lambda x: np.in1d(x, manual_selected_objids), 'OBJID')), \
-                {'TARGETING_SCORE': 100})
+        fill_values_by_query(
+            base,
+            Query((lambda x: np.in1d(x, manual_selected_objids), 'OBJID'), ~C.has_spec),
+            {'TARGETING_SCORE': 100}
+        )
 
     base.sort('TARGETING_SCORE')
     return base
