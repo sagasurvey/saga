92bf820d24ae8e6c41be3408dc7ec908355bab02
marlageha <marla.geha@yale.edu>
2018-06-17 09:41:00 -0400

    Update saga_database.py

--- SAGA/database/saga_database.py
+++ SAGA/database/saga_database.py
@@ -83,11 +83,14 @@ class Database(object):
                                             use_local_first=True),
             'spectra_2dF': DataObject(FitsTable(os.path.join(self._shared_dir, 'Spectra', 'Final', '2dF', '2dF_best.fit'))),
             'spectra_6dF': DataObject(FitsTable(os.path.join(self._shared_dir, 'Spectra', 'Final', '6dF', '6dF_DR3.fit'))),
+            'spectra_2dflen': DataObject(os.path.join(self._shared_dir, 'Spectra', 'Final', '2dF', '2dflens_final.dat')),
+
         }
 
         self._tables['spectra_raw_all'] = DataObject(SpectraData(os.path.join(self._shared_dir, 'Spectra', 'Final'),
                                                                  {'gama': self._tables['spectra_gama_dr3'],
                                                                   '2dF': self._tables['spectra_2dF'],
+                                                                  '2dflen': self._table['spectra_2dflen'],
                                                                   '6dF': self._tables['spectra_6dF'],
                                                                   'ozdes': self._tables['spectra_ozdes_dr1']}))
 
