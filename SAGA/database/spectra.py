031f805763b1a9b8e9e2d1e58b9b32e0d3adf4c1
Yao-Yuan Mao <yymao.astro@gmail.com>
2018-07-02 11:26:46 -0400

    update misc specs

--- SAGA/database/spectra.py
+++ SAGA/database/spectra.py
@@ -335,11 +335,71 @@ def read_ozdes(file_path):
     return ensure_specs_dtype(specs)
 
 
-def read_misc(file_path):
+def read_ukst(file_path):
+    """REFERENCE:  DURHAM UKST
+    http://adsabs.harvard.edu/abs/1998MNRAS.300..417R
+    """
     if not hasattr(file_path, 'read'):
         file_path = FitsTable(file_path)
-    specs = file_path.read()
-    sepcs = specs[np.isfinite(specs['SPEC_Z'])]
+    specs = file_path.read()['_RAJ2000', '_DEJ2000', 'DUGRS', 'RV']
+    specs = specs[specs['RV'] != np.iinfo(np.int32).min]
+
+    specs.rename_column('_RAJ2000', 'RA')
+    specs.rename_column('_DEJ2000', 'DEC')
+    specs.rename_column('DUGRS', 'SPECOBJID')
+
+    specs['SPEC_Z'] = specs['RV'].astype(np.float64) / _SPEED_OF_LIGHT
+    del specs['RV']
+    specs['SPEC_Z_ERR'] = 100 / _SPEED_OF_LIGHT
+    specs['ZQUALITY'] = 4
+    specs['TELNAME'] = 'UKST'
+    specs['HELIO_CORR'] = True
+
+    return ensure_specs_dtype(specs)
+
+
+def read_lcrs(file_path):
+    """REFERENCE:  LCRS
+    http://vizier.cfa.harvard.edu/viz-bin/VizieR-2
+    http://adsabs.harvard.edu/abs/1996ApJ...470..172S
+    """
+    if not hasattr(file_path, 'read'):
+        file_path = FitsTable(file_path)
+    specs = file_path.read()['_RAJ2000', '_DEJ2000', 'p', 'cz', 'e_cz']
+    specs = specs[specs['cz'] != np.iinfo(np.int32).min]
+
+    specs.rename_column('_RAJ2000', 'RA')
+    specs.rename_column('_DEJ2000', 'DEC')
+    specs.rename_column('p', 'SPECOBJID')
+
+    specs['SPEC_Z'] = specs['cz'].astype(np.float64) / _SPEED_OF_LIGHT
+    specs['SPEC_Z_ERR'] = specs['e_cz'].astype(np.float64) / _SPEED_OF_LIGHT
+    del specs['cz'], specs['e_cz']
+
+    specs['ZQUALITY'] = 4
+    specs['TELNAME'] = 'LCRS'
+    specs['HELIO_CORR'] = True
+    return ensure_specs_dtype(specs)
+
+
+def read_slackers(file_path):
+    """REFERENCE:  SLACKERS
+    FILE WAS FOUND IN MG's EMAIL.   UNPUBLISHED SURVEY FROM LAS CAMPANAS 2.5M
+    """
+    if not hasattr(file_path, 'read'):
+        file_path = FitsTable(file_path)
+    specs = file_path.read()['OBJ_RA', 'OBJ_DEC', 'ID', 'WFCCD_ZQUAL', 'WFCCD_Z', 'WFCCD_ZERR']
+    specs = Query('WFCCD_ZQUAL > 2').filter(specs)
+
+    specs.rename_column('OBJ_RA', 'RA')
+    specs.rename_column('OBJ_DEC', 'DEC')
+    specs.rename_column('ID', 'SPECOBJID')
+    specs.rename_column('WFCCD_ZQUAL', 'ZQUALITY')
+    specs.rename_column('WFCCD_Z', 'SPEC_Z')
+    specs.rename_column('WFCCD_ZERR', 'SPEC_Z_ERR')
+
+    specs['TELNAME'] = 'slack'
+    specs['HELIO_CORR'] = True
     return ensure_specs_dtype(specs)
 
 
