2df9f037af36c4668ef6426551b60f359b2479b3
Yao-Yuan Mao <yymao.astro@gmail.com>
2018-09-18 20:06:08 -0400

    better FitsTable

--- SAGA/database/core.py
+++ SAGA/database/core.py
@@ -89,17 +89,21 @@ class FitsTable(FileObject):
     compress_after_write = True
 
     def read(self):
-        with fits.open(self.path, cache=False, lazy_load_hdus=True, **self.kwargs) as f:
-            return Table(f[1].data) # pylint: disable=E1101
+        with fits.open(self.path, cache=False, memmap=False, **self.kwargs) as hdu_list:
+            t = Table.read(hdu_list, hdu=1)
+        return t
 
     def write(self, table):
+        coord = None
         if 'coord' in table.columns and table['coord'].info.dtype.name == 'object':
-            table = table.copy()
+            coord = table['coord']
             del table['coord']
         file_open = gzip.open if self.compress_after_write else open
         makedirs_if_needed(self.path)
         with file_open(self.path, 'wb') as f_out:
             table.write(f_out, format='fits')
+        if coord is not None:
+            table['coord'] = coord
 
 
 class NumpyBinary(FileObject):
