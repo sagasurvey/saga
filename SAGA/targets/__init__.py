b171513b7d95ae86c24edd652cd64cd18212720a
Yao-Yuan Mao <yymao.astro@gmail.com>
2018-09-20 09:41:57 -0400

    update module docstring

--- SAGA/targets/__init__.py
+++ SAGA/targets/__init__.py
@@ -2,8 +2,6 @@
 SAGA.targets
 
 This subpackage contains target-related routines, including TargetSelection
-
-This subpackage is still under construction! DO NOT USE YET!
 """
 from .target_selection import *
 from .assign_targeting_score import *
