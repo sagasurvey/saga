Fix SB calculation again

SAGA/objects/build2.py
@@ -883,7 +883,9 @@ def remove_too_close_to_host(base):
 
 def add_surface_brightness(base):
     radius = np.maximum(base["radius"], 1e-20)
-    base["sb_r"] = base["r_mag"] + 2.5 * np.log10(np.pi * radius * radius)
+    # the factor 2 inside log10 is to account for that the magnitude we are using is total magnitude
+    # but radius is half-light radius
+    base["sb_r"] = base["r_mag"] + 2.5 * np.log10(2 * np.pi * radius * radius)
     base["sb_r_err"] = np.hypot(base["r_err"], (5 / np.log(10)) * base["radius_err"] / radius)
     return base
 

SAGA/objects/object_catalog.py
@@ -27,7 +27,7 @@ def get_unique_objids(objid_col):
     return np.unique(np.asarray(objid_col, dtype=np.int64))
 
 
-def calc_fiducial_p_sat(base, params=(-1.68, 1.25, -5.5, 2.3, 0.5)):
+def calc_fiducial_p_sat(base, params=(-1.7, 1.25, -6, 2, 0.5)):
     gr = np.where(
         C.valid_g_mag.mask(base),
         base["gr"],
@@ -41,7 +41,7 @@ def calc_fiducial_p_sat(base, params=(-1.68, 1.25, -5.5, 2.3, 0.5)):
     sb = np.where(
         C.valid_sb.mask(base),
         base["sb_r"],
-        20 + 0.6 * (base["r_mag"] - 14),
+        20.75 + 0.6 * (base["r_mag"] - 14),
     )
 
     r = base["r_mag"]
@@ -118,8 +118,7 @@ class ObjectCatalog(object):
                     table["{}_err".format(color[0])], table["{}_err".format(color[1])]
                 )
 
-        if "sb_r_err" not in table.colnames:
-            table = build2.add_surface_brightness(table)
+        table = build2.add_surface_brightness(table)  # TODO: remove this line in the future
 
         if "HOST_ID" in table.colnames:
             table.rename_column("HOST_ID", "HOSTID")

SAGA/targets/assign_targeting_score.py
@@ -312,7 +312,7 @@ def assign_targeting_score_v2(
     if "des" in surveys:
         des_bright_stars = Query(
             QueryMaker.equals("survey", "des"),
-            "0.7 * (r_mag + 9.125) > sb_r",
+            "0.7 * (r_mag + 10.2) > sb_r",
             "gr < 0.6",
             "r_mag < 17",
             C.valid_g_mag,
@@ -324,12 +324,12 @@ def assign_targeting_score_v2(
     veryhigh_p_gmm = Query("P_GMM >= 0.95", "log_L_GMM >= -7")
     high_p_gmm = Query("P_GMM >= 0.7") | Query("log_L_GMM < -7")
 
-    low_sb_cut = Query(Query("score_sb_r >= 19.25"), C.valid_sb)
+    low_sb_cut = Query(Query("score_sb_r >= 20"), C.valid_sb)
     very_low_sb_cut = Query(
         "r_mag < 20.8",
         (
-            Query(C.high_priority_cuts, Query("score_sb_r >= 20.5") | Query("sb_r >= 24.5"))
-            | Query(QueryMaker.equals("survey", "des"), Query("score_sb_r >= 20.75") | Query("sb_r >= 24.75"))
+            Query(C.high_priority_cuts, Query("score_sb_r >= 21.25") | Query("sb_r >= 25.25"))
+            | Query(QueryMaker.equals("survey", "des"), Query("score_sb_r >= 21.5") | Query("sb_r >= 25.5"))
         ),
         C.valid_sb,
         exclusion_cuts,
@@ -379,7 +379,7 @@ def assign_targeting_score_v2(
     base["TARGETING_SCORE"][need_random_selection] = 500
 
     base["TARGETING_SCORE"] += (
-        8 - np.digitize(base["score_sb_r"], np.linspace(18.5, 21.5, 7))
+        8 - np.digitize(base["score_sb_r"], np.linspace(19.25, 22, 7))
     ) * 10 + (9 - np.floor(base["P_GMM"] * 10).astype(np.int))
 
     fill_values_by_query(base, ~basic_cut, {"TARGETING_SCORE": 1100})
@@ -439,7 +439,7 @@ def assign_targeting_score_lowz(
     )
     base["pass_r_gr_cut"] = Query("r_mag < (2-gr)*14").mask(base)
     base["pass_gr_ri_cut"] = Query("0.5*gr + 0.05 > ri").mask(base)
-    base["pass_r_sb_cut"] = Query("0.9*r_mag + 4.5 < sb_r").mask(base)
+    base["pass_r_sb_cut"] = Query("0.9*r_mag + 5.25 < sb_r").mask(base)
 
     base["TARGETING_SCORE"] = 1000
     basic = Query("r_mag > 18", "pass_r_gr_cut", "pass_gr_ri_cut", "pass_r_sb_cut")