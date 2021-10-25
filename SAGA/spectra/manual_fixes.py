4ce312f5f31c15edc932d87b149529a07c6fd088
Yao-Yuan Mao <yymao.astro@gmail.com>
2021-10-25 14:16:21 -0400

    add OZDES spec fixes

--- SAGA/spectra/manual_fixes.py
+++ SAGA/spectra/manual_fixes.py
@@ -6,12 +6,16 @@ fixes_sdss_spec_by_objid = {
     1237660635999306305: {"ZQUALITY": 1},
 }
 
-fixes_2df_spec_by_objid = {
-    "TGS905Z093": {"ZQUALITY": 3},
-}
-
 fixes_nsa_v1_spec_by_nsaid = {
     681293: {"ZQUALITY": 1},
     622813: {"ZQUALITY": 1},
     635385: {"ZQUALITY": 1},
+}
+
+fixes_2df_spec_by_objid = {
+    "TGS905Z093": {"ZQUALITY": 3},
+}
+
+fixes_ozdes_spec_by_objid = {
+    "SVA1_COADD-2940383035": {"ZQUALITY": 1},
 }
