62d08d6e33b68fd0cb70db9b6f81549495d5fd9e
Yao-Yuan Mao <yymao.astro@gmail.com>
2021-11-12 21:40:29 -0500

    bug fix

--- SAGA/spectra/read_external.py
+++ SAGA/spectra/read_external.py
@@ -43,7 +43,7 @@ def read_gama(file_path):
 def read_2df(file_path):
     if not hasattr(file_path, "read"):
         file_path = FitsTable(file_path)
-    specs = file_path.read()["RAJ2000", "DEJ2000", "Name", "z", "q_z", "n_z"]
+    specs = file_path.read()["RAJ2000", "DEJ2000", "Name", "z", "q_z"]
 
     # 3 = probably galaxy, 4 = definite galaxy, 6 = confirmed star
     specs = Query("q_z >= 3").filter(specs)
